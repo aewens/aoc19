bf6258dd5fe8ceb23469bf7f69d544b896aef9d1
aewens <email@aewens.com>
2019-12-03 12:53:14 -0500

    Day 3, Part 2

--- aoc3.py
+++ aoc3.py
@@ -2,13 +2,16 @@ from pathlib import Path
 from math import inf
 
 def parse_wires(raw_data):
-    wires = list()
+    steps = list()
+    points = list()
     for raw_wire in raw_data.split():
-        wire = set()
+        wire_steps = dict()
+        wire_points = set()
         curr_point = 0, 0
+        curr_steps = 0
         for turn in raw_wire.split(","):
-            direction, distance = turn[0], turn[1:]
-            for d in range(int(distance)):
+            direction, distance = turn[0], int(turn[1:])
+            for d in range(distance):
                 if direction == "R":
                     next_point = curr_point[0] + 1, curr_point[1]
 
@@ -21,16 +24,26 @@ def parse_wires(raw_data):
                 elif direction == "D":
                     next_point = curr_point[0], curr_point[1] - 1
 
-                wire.add(next_point)
+                curr_steps = curr_steps + 1
+                step_key = ":".join(map(str, next_point))
+                wire_step = wire_steps.get(step_key)
+                if not wire_step:
+                    wire_steps[step_key] = curr_steps
+
+                wire_points.add(next_point)
                 curr_point = next_point
 
-        wires.append(wire)
+        steps.append(wire_steps)
+        points.append(wire_points)
 
-    return wires
+    return points, steps
 
-def solve(wires):
+def get_crosses(wires):
     wire1, wire2 = wires
     cross_points = wire1.intersection(wire2)
+    return cross_points
+
+def closest_cross(cross_points):
     closest = inf
     for cross_point in cross_points:
         cross_x, cross_y = cross_point
@@ -39,7 +52,23 @@ def solve(wires):
 
     return closest
 
+def fewest_steps(cross_points, wire_steps):
+    steps1, steps2 = wire_steps
+    fewest = inf
+    for cross_point in cross_points:
+        step_key = ":".join(map(str, cross_point))
+        step1 = steps1[step_key]
+        step2 = steps2[step_key]
+        steps = step1 + step2
+        fewest = min(steps, fewest)
+
+    return fewest
+
 if __name__ == "__main__":
     raw_data = Path("aoc3.txt").read_text()
-    dist1 = solve(parse_wires(raw_data))
-    print("Part 1:", dist1)
+    wire_points, wire_steps = parse_wires(raw_data)
+    cross_points = get_crosses(wire_points)
+    closest = closest_cross(cross_points)
+    print("Part 1:", closest)
+    fewest = fewest_steps(cross_points, wire_steps)
+    print("Part 2:", fewest)
