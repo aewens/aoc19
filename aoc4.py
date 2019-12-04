10f329b1fb2f7b5a54112dbde83349153674fabb
aewens <email@aewens.com>
2019-12-04 12:33:37 -0500

    Day 4, Part 2

--- aoc4.py
+++ aoc4.py
@@ -1,5 +1,5 @@
 
-def get_valid_passwords(puzzle_input):
+def get_valid_passwords(puzzle_input, limit_adjacents=False):
     start, end = puzzle_input.split("-")
     password_range = map(str, range(int(start), int(end)))
     valid_passwords = list()
@@ -8,11 +8,14 @@ def get_valid_passwords(puzzle_input):
         adjacent = False
         decreases = False
         previous = str(-1)
+        adjacents = dict()
 
         for index in range(len(test_case)):
             digit = test_case[index]
-            if not adjacent and digit == previous:
+            if digit == previous:
                 adjacent = True
+                if limit_adjacents:
+                    adjacents[digit] = adjacents.get(digit, 0) + 1
 
             if int(digit) < int(previous):
                 decreases = True
@@ -21,11 +24,19 @@ def get_valid_passwords(puzzle_input):
             previous = digit
 
         if not decreases and adjacent:
-            valid_passwords.append(test_case)
+            if limit_adjacents:
+                if 1 in adjacents.values():
+                    valid_passwords.append(test_case)
+
+            else:
+                valid_passwords.append(test_case)
 
     return valid_passwords
 
 if __name__ == "__main__":
     puzzle_input = "240920-789857"
-    valid_passwords = get_valid_passwords(puzzle_input)
-    print("Part 1:", len(valid_passwords))
+    valid_passwords1 = get_valid_passwords(puzzle_input)
+    print("Part 1:", len(valid_passwords1))
+
+    valid_passwords2 = get_valid_passwords(puzzle_input, True)
+    print("Part 2:", len(valid_passwords2))
