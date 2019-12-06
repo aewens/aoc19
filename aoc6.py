3c72e9ffe27cabd2c189c153b6fdb656985b9325
aewens <email@aewens.com>
2019-12-06 15:26:37 -0500

    Day 6, Part 2

--- aoc6.py
+++ aoc6.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 from pathlib import Path
 
+# DEBUG
+from pprint import pprint
+
 def parse_map(text_map):
     orbits = text_map.split()
     orbit_map = defaultdict(set)
@@ -10,29 +13,55 @@ def parse_map(text_map):
 
     return orbit_map
 
-def count_orbits(orbit_map, check=None, chain=list(), count=(0, 0)):
-    if check is None:
-        check = orbit_map.get("COM")
-        if check is None:
-            return count
+def count_orbits(orbit_map, graph=dict(), orbits=None, chain=list(), direct=0,
+    indirect=0):
+    if orbits is None:
+        orbits = orbit_map.get("COM")
+        if orbits is None:
+            return direct, indirect
 
-    for orbit in check:
-        direct, indirect = count
+    for orbit in orbits:
         direct = direct + 1
         indirect = indirect + len(chain)
-        new_count = direct, indirect
 
-        new_check = orbit_map.get(orbit)
-        if new_check is not None:
-            new_chain = chain + [orbit]
-            count = count_orbits(orbit_map, new_check, new_chain, new_count)
+        satellites = orbit_map.get(orbit)
+        if satellites is not None:
+            chain_orbit = chain + [orbit]
+            args = orbit_map, graph, satellites, chain_orbit, direct, indirect
+            graph, direct, indirect = count_orbits(*args)
 
         else:
-            count = new_count
+            graph[orbit] = ["COM"] + chain
 
-    return count
+    return graph, direct, indirect
+
+def find_shortest_distance(graph):
+    you = graph.get("YOU")
+    san = graph.get("SAN")
+
+    if None in [you, san]:
+        return -1
+
+    closest_intersection = None
+    you_r = you[::-1]
+    san_r = san[::-1]
+    for orbit in you_r:
+        if orbit in san_r:
+            closest_intersection = orbit
+            break
+
+    you_i = you_r.index(closest_intersection)
+    san_i = san.index(closest_intersection)
+    path = you_r[1:you_i] + san[san_i:]
+
+    return len(path)
 
 if __name__ == "__main__":
     orbit_map = parse_map(Path("aoc6.txt").read_text())
-    total_orbits = sum(count_orbits(orbit_map))
+    #orbit_map = parse_map("COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN")
+    graph, direct_orbits, indirect_orbits = count_orbits(orbit_map)
+    total_orbits = direct_orbits + indirect_orbits
     print("Part 1:", total_orbits)
+
+    distance = find_shortest_distance(graph)
+    print("Part 2:", distance)
