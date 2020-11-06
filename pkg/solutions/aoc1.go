d33d74a590283568f86dfb1ad0793095a2ab316d
aewens <email@aewens.com>
2020-11-06 22:55:44 +0000

    Added solution for Day 1, Part 2

--- pkg/solutions/aoc1.go
+++ pkg/solutions/aoc1.go
@@ -4,15 +4,35 @@ func init() {
 	Map[1] = Solution1
 }
 
+func getFuel(mass int) int {
+	return (mass / 3) - 2
+}
+
+func getFuelFuel(fuel int) int {
+	needed := getFuel(fuel)
+	if needed <= 0 {
+		return 0
+	}
+
+	return needed + getFuelFuel(needed)
+}
+
 func Solution1(lines chan string) {
 	fuel := 0
+	moduleFuel := 0
 	for line := range lines {
 		mass := StringToInt(line)
-		fuel = fuel + ((mass / 3) - 2)
+		neededFuel := getFuel(mass)
+		fuelFuel := getFuelFuel(neededFuel)
+
+		moduleFuel = moduleFuel + neededFuel
+		fuel = fuel + fuelFuel
 
 		if lines == nil {
 			break
 		}
 	}
-	Display(1, IntToString(fuel))
+	
+	Display(1, moduleFuel)
+	Display(2, moduleFuel + fuel)
 }
