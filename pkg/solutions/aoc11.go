558589148d7c838f9a867ad61d88d4e4dbddc9a9
aewens <email@aewens.com>
2020-11-17 19:50:48 +0000

    Added solution for Day 11, Part 2

--- pkg/solutions/aoc11.go
+++ pkg/solutions/aoc11.go
@@ -4,18 +4,6 @@ import (
 	"github.com/aewens/aoc19/pkg/intcode"
 )
 
-/**
- * NOTE: Currently has a bug where it will sometime fail with:
- *  panic: Unknown mode: 10 | X
- *
- * Where X is the current position of the intcode computer. This is from a race
- * condition that I am in no mood to fix right now. Just run it again and it
- * should fix the issue.
- *
- * A potential fix for this would be not using channels and just passing in I/O
- * using a queue or int arrays.
- */
-
 func init() {
 	Map[11] = Solution11
 }
@@ -30,8 +18,6 @@ type Ship struct {
 }
 
 type Robot struct {
-	Input     chan int
-	Output    chan int
 	Computer  *intcode.Computer
 	Direction int
 	X         int
@@ -51,10 +37,10 @@ func (robot *Robot) Step(ship *Ship) bool {
 			ship.Panels[robot.Y][robot.X] = 0
 			panel = 0
 		}
-		go robot.Computer.Input(panel)
+		robot.Computer.Input(panel)
 		robot.Computer.Step()
 	case 4:
-		go robot.Computer.Step()
+		robot.Computer.Step()
 		paint := robot.Computer.Output()
 		ship.Panels[robot.Y][robot.X] = paint
 		_, ok := ship.Paint[robot.Y]
@@ -65,12 +51,12 @@ func (robot *Robot) Step(ship *Ship) bool {
 
 		action := robot.Computer.StepUntil(4, 99)
 		if action == 99 {
+			robot.Computer.Step()
 			return true
 		}
 
-		go robot.Computer.Step()
+		robot.Computer.Step()
 		direction := robot.Computer.Output()
-		//Display(-31, direction)
 		switch direction {
 		case 0:
 			robot.Direction = (robot.Direction - 1) % 4
@@ -86,8 +72,8 @@ func (robot *Robot) Step(ship *Ship) bool {
 		switch robot.Direction {
 		case 0:
 			robot.Y = robot.Y - 1
-			if robot.Y > ship.MaxY {
-				ship.MaxY = robot.Y
+			if robot.Y < ship.MinY {
+				ship.MinY = robot.Y
 			}
 		case 1:
 			robot.X = robot.X + 1
@@ -96,8 +82,8 @@ func (robot *Robot) Step(ship *Ship) bool {
 			}
 		case 2:
 			robot.Y = robot.Y + 1
-			if robot.Y < ship.MinY {
-				ship.MinY = robot.Y
+			if robot.Y > ship.MaxY {
+				ship.MaxY = robot.Y
 			}
 		case 3:
 			robot.X = robot.X - 1
@@ -108,6 +94,7 @@ func (robot *Robot) Step(ship *Ship) bool {
 			panic("Invalid robot direction")
 		}
 	case 99:
+		robot.Computer.Step()
 		return true
 	}
 
@@ -115,9 +102,9 @@ func (robot *Robot) Step(ship *Ship) bool {
 }
 
 func (robot *Robot) Display(ship *Ship) {
-	Clear()
+	//Clear()
 
-	for y := ship.MinY; y < ship.MaxY; y++ {
+	for y := ship.MinY; y < ship.MaxY + 1; y++ {
 		row := ""
 		for x := ship.MinX; x < ship.MaxX; x++ {
 			panel := ship.Panels[y][x]
@@ -127,36 +114,39 @@ func (robot *Robot) Display(ship *Ship) {
 				row = row + " "
 			}
 		}
-		Display(0, row)
+		Display(2, row)
 	}
 }
 
-func (robot *Robot) Run(ship *Ship) int {
-	done := make(chan bool)
+func (robot *Robot) Reset() {
+	robot.X = 0
+	robot.Y = 0
+	robot.Direction = 0
+	robot.Computer.Reset()
+}
 
-	go func() {
-		for {
-			halted := robot.Step(ship)
-			if halted {
-				done <- true
-				break
-			}
+func (robot *Robot) Run(ship *Ship, display bool) int {
+	for {
+		halted := robot.Step(ship)
+		if halted {
+			break
 		}
-	}()
+	}
 
-	<-done
 	panels := 0
 	for _, row := range ship.Paint {
 		panels = panels + len(row)
 	}
-	//robot.Display(ship)
+	if display {
+		robot.Display(ship)
+	}
 
 	return panels
 }
 
 func Solution11(lines chan string) {
 	program := <-lines
-	computer := intcode.BufferedNew(program)
+	computer := intcode.QueuedNew(program)
 	ship := &Ship{
 		MinX:   0,
 		MinY:   0,
@@ -166,14 +156,25 @@ func Solution11(lines chan string) {
 		Paint:  make(map[int]map[int]bool),
 	}
 	robot := &Robot{
-		Input:     make(chan int),
-		Output:    make(chan int),
 		Computer:  computer,
 		Direction: 0,
 		X:         0,
 		Y:         0,
 	}
 
-	panels := robot.Run(ship)
+	panels := robot.Run(ship, false)
 	Display(1, panels)
+
+	robot.Reset()
+	ship = &Ship{
+		MinX:   0,
+		MinY:   0,
+		MaxX:   0,
+		MaxY:   0,
+		Panels: make(map[int]map[int]int),
+		Paint:  make(map[int]map[int]bool),
+	}
+	ship.Panels[0] = make(map[int]int)
+	ship.Panels[0][0] = 1
+	robot.Run(ship, true)
 }
